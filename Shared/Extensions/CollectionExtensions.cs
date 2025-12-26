19d69dcc05c4da1caa7ed42c68dd94491b2ffadc
Van Anh Nguyen <107466828+VanAnh271000@users.noreply.github.com>
2025-12-26 10:32:53 +0700

    #3 add extensions

---
+++ Shared/Extensions/CollectionExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Shared.Results;
+
+namespace Shared.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || !enumerable.Any();
+        }
+
+        public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T> enumerable) where T : class
+        {
+            return enumerable?.Where(item => item != null) ?? Enumerable.Empty<T>();
+        }
+
+        public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> enumerable, Func<T, TKey> keySelector)
+        {
+            var knownKeys = new HashSet<TKey>();
+            return enumerable.Where(element => knownKeys.Add(keySelector(element)));
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable)
+            {
+                action(item);
+            }
+        }
+
+        public static IEnumerable<T> TakePage<T>(this IEnumerable<T> enumerable, int pageNumber, int pageSize)
+        {
+            return enumerable.Skip((pageNumber) * pageSize).Take(pageSize);
+        }
+
+        public static PagedResult<T> ToPagedResult<T>(this IEnumerable<T> enumerable, int pageNumber, int pageSize)
+        {
+            var totalCount = enumerable.Count();
+            var items = enumerable.TakePage(pageNumber, pageSize);
+
+            return new PagedResult<T>(items, totalCount, pageNumber, pageSize);
+        }
+
+        public static bool HasItems<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable != null && enumerable.Any();
+        }
+
+        public static T SafeFirst<T>(this IEnumerable<T> enumerable) where T : class
+        {
+            return enumerable?.FirstOrDefault();
+        }
+
+        public static T SafeLast<T>(this IEnumerable<T> enumerable) where T : class
+        {
+            return enumerable?.LastOrDefault();
+        }
+    }
+}
---
+++ Shared/Extensions/QueryableExtensions.cs
@@ -0,0 +1,242 @@
+﻿using Shared.QueryParameter;
+using Shared.Results;
+using System.Linq.Expressions;
+
+namespace Shared.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> ApplyFilters<T>(this IQueryable<T> query, List<FilterParameter> filters)
+        {
+            if (!filters.Any()) return query;
+
+            var parameter = Expression.Parameter(typeof(T), "x");
+            Expression? combinedExpression = null;
+
+            foreach (var filter in filters)
+            {
+                var propertyExpression = GetPropertyExpression(parameter, filter.Field);
+                if (propertyExpression == null) continue;
+
+                var filterExpression = BuildFilterExpression(propertyExpression, filter);
+                if (filterExpression == null) continue;
+
+                if (combinedExpression == null)
+                {
+                    combinedExpression = filterExpression;
+                }
+                else
+                {
+                    combinedExpression = filter.LogicalOperator.ToLower() == "or"
+                        ? Expression.OrElse(combinedExpression, filterExpression)
+                        : Expression.AndAlso(combinedExpression, filterExpression);
+                }
+            }
+
+            if (combinedExpression != null)
+            {
+                var lambda = Expression.Lambda<Func<T, bool>>(combinedExpression, parameter);
+                query = query.Where(lambda);
+            }
+
+            return query;
+        }
+
+        public static IQueryable<T> ApplySearch<T>(this IQueryable<T> query, string? searchTerm, params string[] searchProperties)
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm) || !searchProperties.Any())
+                return query;
+
+            var parameter = Expression.Parameter(typeof(T), "x");
+            Expression? searchExpression = null;
+            var searchValue = Expression.Constant(searchTerm, typeof(string));
+            var containsMethod = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+
+            foreach (var propertyName in searchProperties)
+            {
+                var propertyExpression = GetPropertyExpression(parameter, propertyName);
+                if (propertyExpression == null)
+                    continue;
+
+                Expression? stringExpression = null;
+
+                // Hỗ trợ string
+                if (propertyExpression.Type == typeof(string))
+                {
+                    stringExpression = propertyExpression;
+                }
+                // Hỗ trợ decimal
+                else if (propertyExpression.Type == typeof(decimal) || propertyExpression.Type == typeof(decimal?))
+                {
+                    stringExpression = Expression.Call(
+                        propertyExpression,
+                        typeof(object).GetMethod("ToString", Type.EmptyTypes)!
+                    );
+                }
+
+                if (stringExpression == null) continue;
+
+                var containsCall = Expression.Call(stringExpression, containsMethod!, searchValue);
+                searchExpression = searchExpression == null
+                    ? containsCall
+                    : Expression.OrElse(searchExpression, containsCall);
+            }
+
+            if (searchExpression != null)
+            {
+                var lambda = Expression.Lambda<Func<T, bool>>(searchExpression, parameter);
+                query = query.Where(lambda);
+            }
+
+            return query;
+        }
+
+
+        public static IQueryable<T> ApplySorting<T>(this IQueryable<T> query, string? sortBy, string sortDirection = "asc")
+        {
+            if (string.IsNullOrWhiteSpace(sortBy)) return query;
+
+            var parameter = Expression.Parameter(typeof(T), "x");
+            var propertyExpression = GetPropertyExpression(parameter, sortBy);
+
+            if (propertyExpression == null) return query;
+
+            var lambda = Expression.Lambda(propertyExpression, parameter);
+            var methodName = sortDirection.ToLower() == "desc" ? "OrderByDescending" : "OrderBy";
+
+            var resultExpression = Expression.Call(
+                typeof(Queryable),
+                methodName,
+                new Type[] { typeof(T), propertyExpression.Type },
+                query.Expression,
+                Expression.Quote(lambda)
+            );
+
+            return query.Provider.CreateQuery<T>(resultExpression);
+        }
+
+        public static PagedResult<T> ToPagedResult<T>(this IQueryable<T> query, int page, int pageSize)
+        {
+            var totalCount = query.Count();
+            var items = query
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return new PagedResult<T>
+            {
+                Data = items,
+                TotalCount = totalCount,
+                Index = page,
+                PageSize = pageSize,
+                TotalPages = (int)Math.Ceiling((double)totalCount / pageSize)
+            };
+        }
+
+        //public static async Task<ReportResult<T>> ToReportResultAsync<T>(this IQueryable<T> query)
+        //{
+        //    var items = query.ToList();
+        //    return new ReportResult<T>
+        //    {
+        //        Data = items
+        //    };
+        //}
+
+        private static Expression? GetPropertyExpression(ParameterExpression parameter, string propertyName)
+        {
+            try
+            {
+                return propertyName.Split('.').Aggregate<string, Expression>(parameter, Expression.PropertyOrField);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static Expression? BuildFilterExpression(Expression propertyExpression, FilterParameter filter)
+        {
+            if (filter.Value == null) return null;
+            var targetType = Nullable.GetUnderlyingType(propertyExpression.Type) ?? propertyExpression.Type;
+            if (filter.Operator.Equals("in", StringComparison.OrdinalIgnoreCase))
+            {
+                // Ép filter.Value thành IEnumerable (ví dụ List<int>)
+                if (filter.Value is System.Collections.IEnumerable enumerable && !(filter.Value is string))
+                {
+                    // Convert từng phần tử về targetType
+                    var list = Activator.CreateInstance(typeof(List<>).MakeGenericType(targetType))!;
+                    var addMethod = list.GetType().GetMethod("Add")!;
+
+                    foreach (var item in enumerable)
+                    {
+                        var convertedItem = Convert.ChangeType(item, targetType);
+                        addMethod.Invoke(list, new[] { convertedItem });
+                    }
+
+                    // Tạo constant expression cho list
+                    var listConstant = Expression.Constant(list);
+
+                    // Gọi Enumerable.Contains(list, property)
+                    var containsMethod = typeof(Enumerable)
+                    .GetMethods()
+                    .First(m => m.Name == "Contains" && m.GetParameters().Length == 2)
+                    .MakeGenericMethod(targetType);
+
+                    Expression memberToCompare = propertyExpression;
+                    if (Nullable.GetUnderlyingType(propertyExpression.Type) != null)
+                    {
+                        memberToCompare = Expression.Property(propertyExpression, "Value");
+                    }
+
+                    return Expression.Call(containsMethod, listConstant, memberToCompare);
+                }
+
+                throw new InvalidOperationException("IN operator requires a list of values");
+            }
+            //var constantExpression = Expression.Constant(Convert.ChangeType(filter.Value, propertyExpression.Type));
+            object? convertedValue;
+
+            try
+            {
+
+                // Nếu là GUID
+                if (targetType == typeof(Guid))
+                {
+                    convertedValue = Guid.Parse(filter.Value.ToString());
+                }
+                // Nếu là Enum
+                else if (targetType.IsEnum)
+                {
+                    convertedValue = Enum.Parse(targetType, filter.Value.ToString()!);
+                }
+                else
+                {
+                    convertedValue = Convert.ChangeType(filter.Value, targetType);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Cannot convert '{filter.Value}' to {propertyExpression.Type}", ex);
+            }
+
+            var constantExpression = Expression.Constant(convertedValue, propertyExpression.Type);
+
+            return filter.Operator.ToLower() switch
+            {
+                "==" => Expression.Equal(propertyExpression, constantExpression),
+                "!=" => Expression.NotEqual(propertyExpression, constantExpression),
+                ">" => Expression.GreaterThan(propertyExpression, constantExpression),
+                "<" => Expression.LessThan(propertyExpression, constantExpression),
+                ">=" => Expression.GreaterThanOrEqual(propertyExpression, constantExpression),
+                "<=" => Expression.LessThanOrEqual(propertyExpression, constantExpression),
+                "contains" when propertyExpression.Type == typeof(string) =>
+                    Expression.Call(propertyExpression, typeof(string).GetMethod("Contains", new[] { typeof(string) })!, constantExpression),
+                "startswith" when propertyExpression.Type == typeof(string) =>
+                    Expression.Call(propertyExpression, typeof(string).GetMethod("StartsWith", new[] { typeof(string) })!, constantExpression),
+                "endswith" when propertyExpression.Type == typeof(string) =>
+                    Expression.Call(propertyExpression, typeof(string).GetMethod("EndsWith", new[] { typeof(string) })!, constantExpression),
+                _ => null
+            };
+        }
+    }
+}
