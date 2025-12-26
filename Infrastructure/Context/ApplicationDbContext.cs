b4c2dbc723af3cad8a8e945bf9acbc47d13b87a4
Van Anh Nguyen <107466828+VanAnh271000@users.noreply.github.com>
2025-12-26 15:44:54 +0700

    #11 Configure db context DI identity in Program.cs

--- Infrastructure/Context/ApplicationDbContext.cs
+++ Infrastructure/Context/ApplicationDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) { }
