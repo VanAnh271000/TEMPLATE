79c68364450a1053bc4525837dea650b32b6eda2
Van Anh Nguyen <107466828+VanAnh271000@users.noreply.github.com>
2025-12-26 10:40:48 +0700

    #4 Create a base entity & auditing

---
+++ Domain/Commons/IAuditableEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Commons
+{
+    public interface IAuditableEntity
+    {
+        DateTime? CreatedTime { get; set; }
+        string? CreatedById { get; set; }
+        string? CreatedByName { get; set; }
+        DateTime? UpdatedTime { get; set; }
+        string? UpdatedById { get; set; }
+        string? UpdatedByName { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedTime { get; set; }
+    }
+
+}
