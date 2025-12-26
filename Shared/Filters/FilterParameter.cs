adc79260df0209d7bcc4ed2d836b70b2cc876e94
Van Anh Nguyen <107466828+VanAnh271000@users.noreply.github.com>
2025-12-26 10:31:05 +0700

    #3 add query parameters

---
+++ Shared/Filters/FilterParameter.cs
@@ -0,0 +1,25 @@
+﻿namespace Shared.QueryParameter
+{
+    public class FilterParameter
+    {
+        public string Field { get; set; } = string.Empty;
+        public string Operator { get; set; } = "=="; 
+        public object? Value { get; set; }
+        public string LogicalOperator { get; set; } = "and"; // and, or
+    }
+
+    public enum FilterOperator
+    {
+        Equal,
+        NotEqual,
+        GreaterThan,
+        LessThan,
+        GreaterThanOrEqual,
+        LessThanOrEqual,
+        Contains,
+        StartsWith,
+        EndsWith,
+        In,
+        NotIn
+    }
+}
---
+++ Shared/Filters/GenericQueryParameters.cs
@@ -0,0 +1,19 @@
+﻿namespace Shared.QueryParameter
+{
+    public class GenericQueryParameters : BaseQueryParameters
+    {
+        public List<FilterParameter> Filters { get; set; } = new();
+
+        public GenericQueryParameters AddFilter(string propertyName, string operatorType, object? value, string logicalOperator = "and")
+        {
+            Filters.Add(new FilterParameter
+            {
+                Field = propertyName,
+                Operator = operatorType,
+                Value = value,
+                LogicalOperator = logicalOperator
+            });
+            return this;
+        }
+    }
+}
