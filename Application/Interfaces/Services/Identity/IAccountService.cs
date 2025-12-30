b30e1527bcc9431ef10f9e03ca76eed11b046d47
Van Anh Nguyen <107466828+VanAnh271000@users.noreply.github.com>
2025-12-30 16:08:28 +0700

    #18 add get list

--- Application/Interfaces/Services/Identity/IAccountService.cs
+++ Application/Interfaces/Services/Identity/IAccountService.cs
@@ -1,9 +1,11 @@
-﻿using Application.DTOs.Identity;
+﻿using Application.DTOs.Commons;
+using Application.DTOs.Identity;
 using Shared.Results;
 
 namespace Application.Interfaces.Services.Identity
 {
     public interface IAccountService
     {
+        ServiceResult<PagedResult<AccountDto>> GetList(CommonQueryParameters parameters);
     }
 }
--- Application/Services/Identity/AccountService.cs
+++ Application/Services/Identity/AccountService.cs
@@ -1,22 +1,31 @@
-﻿using Application.DTOs.Identity;
+﻿using Application.DTOs.Commons;
+using Application.DTOs.Identity;
 using Application.Interfaces.Commons;
+using Application.Interfaces.Queries;
 using Application.Interfaces.Services.Identity;
 using Application.Services.Commons;
 using AutoMapper;
 using Domain.Entities.Identity;
 using Microsoft.AspNetCore.Identity;
+using Shared.Constants;
 using Shared.Results;
+using System.Data;
 
 namespace Application.Services.Identity
 {
     public class AccountService : GenericService<ApplicationUser, AccountDto, CreateAccountDto, string>, IAccountService
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserQuery _userQuery;
 
-        public AccountService(IGenericRepository<ApplicationUser, string> repository, IMapper mapper, IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager)
+        public AccountService(IGenericRepository<ApplicationUser, string> repository, 
+            IMapper mapper, IUnitOfWork unitOfWork, 
+            UserManager<ApplicationUser> userManager,
+            IUserQuery userQuery)
             : base(repository, mapper, unitOfWork)
         {
             _userManager = userManager;
+            _userQuery = userQuery;
         }
 
         public async override Task<ServiceResult<AccountDto>> CreateAsync(CreateAccountDto createAccountDto)
@@ -44,5 +53,21 @@ namespace Application.Services.Identity
                 return ServiceResult<AccountDto>.InternalServerError($"An error occurred while creating the user: {ex.Message}");
             }
         }
+
+        public ServiceResult<PagedResult<AccountDto>> GetList(CommonQueryParameters parameters)
+        {
+            try
+            {
+                var result = _userQuery.GetList(parameters, new[] { "UserName", "FullName", "Department.Name" });
+                if (result == null)
+                    return ServiceResult<PagedResult<AccountDto>>.NotFound(ErrorMessages.UserNotFound);
+                return ServiceResult<PagedResult<AccountDto>>.Success(result);
+            }
+            catch (Exception ex)
+            {
+                return ServiceResult<PagedResult<AccountDto>>.InternalServerError($"{ErrorMessages.GetAccountFailed}: {ex.Message}");
+            }
+        }
+
     }
 }
