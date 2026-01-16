db093da9eb0ac794f58617e8b911fb7152e66628
Van Anh Nguyen <107466828+VanAnh271000@users.noreply.github.com>
2026-01-16 16:56:17 +0700

    #40 Expose notification service for application layer

---
+++ Infrastructure/BackgroundJobs/Services/NotificationService.cs
@@ -0,0 +1,23 @@
+﻿using Application.DTOs.Notification;
+using Application.Interfaces.Services.Hangfire;
+using Application.Interfaces.Services.Notification;
+using Infrastructure.BackgroundJobs.Jobs;
+
+namespace Infrastructure.BackgroundJobs.Services
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly IBackgroundJobService _jobService;
+
+        public NotificationService(IBackgroundJobService jobService)
+        {
+            _jobService = jobService;
+        }
+
+        public Task SendAsync(NotificationMessage message)
+        {
+            _jobService.Enqueue<NotificationJob>(job => job.ExecuteAsync(message));
+            return Task.CompletedTask;
+        }
+    }
+}
