652a4f452be35708c02b35500d6778858ec35e15
Van Anh Nguyen <107466828+VanAnh271000@users.noreply.github.com>
2026-01-16 16:47:55 +0700

    #38 add email notification sender

---
+++ Application/Services/Notification/EmailNotificationSender .cs
@@ -0,0 +1,23 @@
+﻿using Application.DTOs.Notification;
+using Application.Interfaces.Services.Notification;
+using Serilog;
+
+namespace Application.Services.Notification
+{
+    public class NotificationSender : INotificationSender
+    {
+        public NotificationChannel Channel => NotificationChannel.Email;
+
+        public Task SendAsync(NotificationMessage message, CancellationToken ct = default)
+        {
+            Log.Information(
+                "Sending email to {RecipientId}. Subject={Subject}",
+                message.RecipientId,
+                message.Subject);
+
+            // TODO: integrate SMTP / provider
+
+            return Task.CompletedTask;
+        }
+    }
+}
