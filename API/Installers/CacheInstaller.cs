c0020d0c632af0b72079ecd5824f9035a0d71fb5
Van Anh Nguyen <107466828+VanAnh271000@users.noreply.github.com>
2026-01-19 15:57:59 +0700

    #53 DI redis service

--- API/Installers/CacheInstaller.cs
+++ API/Installers/CacheInstaller.cs
@@ -1,4 +1,5 @@
-﻿using Application.Interfaces.Services.Caching;
+﻿using Application.DTOs.Configuration;
+using Application.Interfaces.Services.Caching;
 using Infrastructure.Caching;
 
 namespace API.Installers
@@ -7,8 +8,16 @@ namespace API.Installers
     {
         public void InstallServices(IServiceCollection services, IConfiguration configuration)
         {
+            //In-Memory Cache
             services.AddMemoryCache();
             services.AddScoped<ICacheService, CacheService>();
+
+            //Redis Cache
+            var redisConfig = configuration.GetSection("Redis").Get<RedisConfiguration>();
+            services.AddSingleton(redisConfig);
+            
+            services.AddSingleton(new RedisConnection(redisConfig.ConnectionString!));
+            services.AddScoped<ICacheService, RedisCacheService>();
         }
     }
 }
---
+++ Application/DTOs/Configuration/RedisConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs.Configuration
+{
+    public class RedisConfiguration
+    {
+        public string ConnectionString { get; set; } = string.Empty;    
+        public string InstanceName { get; set; } = string.Empty;
+    }
+}
