02ff751d6b18357d2b50acd2a1f7dc3de3cf06de
Van Anh Nguyen <107466828+VanAnh271000@users.noreply.github.com>
2026-01-19 15:12:27 +0700

    #49 add user cache key

---
+++ Application/DTOs/CacheKeys/UserCacheKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.DTOs.CacheKeys
+{
+    public class UserCacheKeys
+    {
+        public static string UserById(string userId)
+        => $"user:entity:{userId}";
+
+        public static string UserQuery(string queryHash)
+            => $"user:query:{queryHash}";
+    }
+}
