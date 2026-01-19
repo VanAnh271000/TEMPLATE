8b4aea2f9183f3a71d968f0bc4d24896c0ddef35
Van Anh Nguyen <107466828+VanAnh271000@users.noreply.github.com>
2026-01-19 09:48:11 +0700

    #43 implement firebase channel

--- Infrastructure/Notifications/FirebaseNotificationChannel.cs
+++ Infrastructure/Notifications/FirebaseNotificationChannel.cs
@@ -1,6 +1,32 @@
-﻿namespace Infrastructure.Notifications
+﻿using Application.DTOs.Notification;
+using Application.Interfaces.Services.Notification;
+using Application.Interfaces.Services.Notification.Senders;
+
+namespace Infrastructure.Notifications
 {
-    public class FirebaseNotificationChannel
+    public class FirebaseNotificationChannel : INotificationChannel
     {
+        private readonly IFirebaseSender _firebaseSender;
+
+        public FirebaseNotificationChannel(IFirebaseSender firebaseSender)
+        {
+            _firebaseSender = firebaseSender;
+        }
+
+        public bool CanHandle(NotificationMessage message)
+            => message.FirebaseToken != null;
+
+        public async Task SendAsync(NotificationMessage message)
+        {
+            await _firebaseSender.SendAsync(new FirebaseNotification()
+            {
+                Body = message.Content,
+                Title = message.Title,
+                DeviceToken = message.FirebaseToken!,
+                Data = message.Metadata != null
+                    ? new Dictionary<string, string>(message.Metadata)
+                    : null
+            });
+        }
     }
 }
