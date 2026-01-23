95f3c5ca99ea765c7261a2c5d623dda6e72a5f38
Van Anh Nguyen <107466828+VanAnh271000@users.noreply.github.com>
2026-01-23 08:53:50 +0700

    #66 Define Application Exceptions

---
+++ Application/Exceptions/AppException.cs
@@ -0,0 +1,30 @@
+﻿namespace Application.Exceptions
+{
+    public abstract class AppException : Exception
+    {
+        protected AppException(string message) : base(message) { }
+    }
+
+    public sealed class AppNotFoundException : AppException
+    {
+        public AppNotFoundException(string message) : base(message) { }
+    }
+
+    public sealed class AppConflictException : AppException
+    {
+        public AppConflictException(string message) : base(message) { }
+    }
+    
+    public sealed class AppValidationException : AppException
+    {
+        public IDictionary<string, string[]> Errors { get; }
+
+        public AppValidationException(
+            IDictionary<string, string[]> errors)
+            : base("Request validation failed")
+        {
+            Errors = errors;
+        }
+    }
+
+}
